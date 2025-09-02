5a6c0d4118ffbfd8e4497adb4663703699b29f8a
kiran <kiranannajagtap@gmail.com>
2025-09-02 17:42:08 +0530

    added new buildspec.yml

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello this is version 2 removed appspec.yaml and taskdef.json"
+    return "Hello added new buildspec.yml"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
